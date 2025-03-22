fb65982836e3e0831f1cf84f4f04a82eb12e58bd
Niko Gorjan <niko.gorjan@gmail.com>
2025-03-22 05:21:28 +0100

    feat(next.config): add output standalone

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
@@ -10,9 +9,8 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item)
-
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
@@ -22,6 +20,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   redirects,
+  output: 'standalone', // Add this line to enable standalone output
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
